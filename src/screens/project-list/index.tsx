fb0d1c34cfeb4d8fe862ffc681e39d0a9274cb64
paranoidyang <paranoidyang@163.com>
2022-10-23 22:39:47 +0800

    chore: 添加ts类型定义

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,39 +6,44 @@ import { cleanObject, useDebounce, useMount } from "../../utils";
 // 1、qs.stringify()：将对象序列化为url查询参数，如：将{key1: value1, key2: value2}序列化为"key1:value1&key2=value2"的形式
 // 2、qs.parse()：将url参数解析成对象
 import * as qs from "qs";
+console.log("qs.stringify('123')", qs.stringify("123"));
 
 // 根据运行环境自动匹配接口域名
-const apiUrl = process.env.REACT_APP_API_URL
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
-    name: '',
-    personId: ''
-  })
-  const debouncedParam = useDebounce(param, 2000)
-  const [list, setList] = useState([])
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+  const [list, setList] = useState([]);
 
   // 监听一个防抖的参数，达到请求防抖的目的
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
       if (response.ok) {
-        setList(await response.json())
+        setList(await response.clone().json());
       }
-    })
-  }, [debouncedParam])
+    });
+  }, [debouncedParam]);
 
   // 类似类组件的componentDidMount
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
-        setUsers(await response.json())
+        setUsers(await response.clone().json());
       }
-    })
-  })
+    });
+  });
 
-  return <div>
-    <SearchPanel users={users} param={param} setParam={setParam} />
-    <List users={users} list={list} />
-  </div>
-}
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !!value);
+export const isFalsy = (value: any) => (value === 0 ? true : !!value);
 
 /**
  * 清除对象的空属性，如
  * @param {*} object
  * @returns
  */
-export const cleanObject = (object) => {
+export const cleanObject = (object: Object) => {
   // 在一个函数里，改变传入的对象本身是不好的，所以这里使用深拷贝
   // 以下代码类似于：Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // 0
+    // @ts-ignore 暂时忽略ts错误
     const value = result[key];
-    if (isFalsy(value)) {
+    if (!isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
@@ -25,7 +26,7 @@ export const cleanObject = (object) => {
  * 模拟类组件的componentDidMount，不用老是写后面的空数组
  * @param {*} callback
  */
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
@@ -34,10 +35,10 @@ export const useMount = (callback) => {
 /**
  * 参数防抖，控制变化的频率
  * @param {*} value
- * @param {*} delay
+ * @param {*} delay 要么不传，要传就传number
  * @returns
  */
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
