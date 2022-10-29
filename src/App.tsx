ee85369ddfc10aa4a58ee614097355d9b2dfaa38
paranoidyang <paranoidyang@163.com>
2022-10-29 20:59:50 +0800

    feat: 添加登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { ProjectListScreen } from "screens/project-list";
-import { TsReactTest } from "try-use-array";
+// import { ProjectListScreen } from "screens/project-list";
+// import { TsReactTest } from "try-use-array";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
       {/* <ProjectListScreen /> */}
-      <TsReactTest />
+      {/* <TsReactTest /> */}
+      <LoginScreen></LoginScreen>
     </div>
   );
 }
