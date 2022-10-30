875e338815bfc72d45bd6b8b3d937f6b0b160419
paranoidyang <paranoidyang@163.com>
2022-10-30 15:28:15 +0800

    feat: 添加登录注册系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
-// import { ProjectListScreen } from "screens/project-list";
-// import { TsReactTest } from "try-use-array";
-import { LoginScreen } from "screens/login";
+import { useAuth } from "context/auth-context";
+import { AuthenticatedApp } from "authenticated-app";
+import { UnauthenticatedApp } from "unauthenticated-app";
 
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      {/* <TsReactTest /> */}
-      <LoginScreen></LoginScreen>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
