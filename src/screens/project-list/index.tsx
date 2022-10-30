bb0602b7525beaf06b037510572e4af86e369c09
paranoidyang <paranoidyang@163.com>
2022-10-30 22:15:47 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,11 +2,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-// react脚手架自带qs：
-// 1、qs.stringify()：将对象序列化为url查询参数，如：将{key1: value1, key2: value2}序列化为"key1:value1&key2=value2"的形式
-// 2、qs.parse()：将url参数解析成对象
-import * as qs from "qs";
-console.log("qs.stringify('123')", qs.stringify("123"));
+import { useHttp } from "utils/http";
 
 // 根据运行环境自动匹配接口域名
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -19,25 +15,16 @@ export const ProjectListScreen = () => {
   });
   const debouncedParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   // 监听一个防抖的参数，达到请求防抖的目的
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.clone().json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   // 类似类组件的componentDidMount
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.clone().json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
---
+++ src/utils/http.ts
@@ -0,0 +1,57 @@
+import qs from "qs";
+// react脚手架自带qs：
+// 1、qs.stringify()：将对象序列化为url查询参数，如：将{key1: value1, key2: value2}序列化为"key1:value1&key2=value2"的形式
+// 2、qs.parse()：将url参数解析成对象
+import * as auth from "auth-provider";
+import { useAuth } from "context/auth-context";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config = {}
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  // axios 和 fetch 的表现不一样，axios可以直接在返回状态不为2xx的时候抛出异常
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
+
+export const useHttp = () => {
+  const { user } = useAuth();
+  // TODO 讲解 TS 操作符
+  return (...[endpoint, config]: Parameters<typeof http>) =>
+    http(endpoint, { ...config, token: user?.token });
+};
