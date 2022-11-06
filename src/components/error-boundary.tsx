0eb65aef11e70e865ce4531e0ec120eeb8f8e190
paranoidyang <paranoidyang@163.com>
2022-11-07 00:53:05 +0800

    feat: 添加errorboundary

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+
+// https://github.com/bvaughn/react-error-boundary
+// React.PropsWithChildren是一个官方泛型，表示children与自定义参数的联合
+// 原理：type PropsWithChildren<P> = P & {children?: ReactNode}
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  // 当子组件抛出异常，这里会接收到并且调用
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
