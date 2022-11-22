9241ddf93f0125ea2607f8acee5d55aeb3e34d5d
paranoidyang <paranoidyang@163.com>
2022-11-22 18:01:43 +0800

    style: 优化kanban样式

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -9,7 +9,12 @@ import {
   useProjectModal,
   useProjectsSearchParams,
 } from "screens/project-list/util";
-import { ButtonNoPadding, ErrorBox, Row } from "components/lib";
+import {
+  ButtonNoPadding,
+  ErrorBox,
+  Row,
+  ScreenContainer,
+} from "components/lib";
 
 // 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
 // https://codesandbox.io/s/keen-wave-tlz9s?file=/src/App.js
@@ -23,7 +28,7 @@ export const ProjectListScreen = () => {
   const { data: users } = useUsers();
 
   return (
-    <Container>
+    <ScreenContainer>
       <Row between={true}>
         <h1>项目列表</h1>
         <ButtonNoPadding onClick={open} type={"link"}>
@@ -33,13 +38,9 @@ export const ProjectListScreen = () => {
       <SearchPanel users={users || []} param={param} setParam={setParam} />
       <ErrorBox error={error} />
       <List loading={isLoading} users={users || []} dataSource={list || []} />
-    </Container>
+    </ScreenContainer>
   );
 };
 
 // 当设为true时，可以直接定位出无限渲染的原因
 ProjectListScreen.whyDidYouRender = false;
-
-const Container = styled.div`
-  padding: 3.2rem;
-`;
